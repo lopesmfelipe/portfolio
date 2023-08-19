ef7f5cb04b45cbebd592120718be3ce4b23435cb
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-08-18 23:44:42 -0400

    Added 'switch' section.

---
+++ SECTIONS/switch/script.js
@@ -0,0 +1,18 @@
+//Switch
+
+x = 2;
+
+switch(x){
+
+    case 0:
+        alert("x = " + x);
+        break;
+    case 1:
+        alert("x = " + x);
+        break;
+    case 2:
+        alert("x = " + x);
+        break;
+
+
+}
