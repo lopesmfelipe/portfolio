edf52eaf9086f49743bd3be1ad84f507b5335aa3
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-08-18 23:57:18 -0400

    update.

---
+++ SECTIONS/switch/switch.js
@@ -0,0 +1,28 @@
+//Switch
+
+document.write("</br> Escolha seu pedido: </br>");
+document.write("</br> 0 - Computer </br>");
+document.write("</br> 2 - Laptop </br>");
+
+
+function order() {
+
+    x = prompt("What is the order?");
+
+    switch(x){
+    
+        case "0":
+            alert("x = " + x);
+            break;
+        case "1":
+            alert("x = " + x);
+            break;
+        case "2":
+            alert("x = " + x);
+            break;
+    
+    }
+
+
+
+}
