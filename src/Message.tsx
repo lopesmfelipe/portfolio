ea74262bc6ad516c0f4beb4f0c3d6f6ea3508321
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-12-20 18:29:05 -0400

    Deleted files

--- src/Message.tsx
+++
@@ -1,9 +0,0 @@
-function Message() {
-  const name = '';
-  const name2 = 'HAYKE'
-  if (name)
-    return <h1>The World is yours {name}</h1>;
-  return <h1>The world is yours {name2}</h1>  
-}
-
-export default Message;
