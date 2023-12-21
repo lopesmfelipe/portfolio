c057e074add2f06c951cd42b2821e932e01db70e
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-12-21 15:57:51 -0400

    Conditional render

--- NEO20/src/App.jsx
+++ NEO20/src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  const age = 15;
+  const isPurple = true;
+
   return (
-    <div>
-      <h1 className="name"> Hokuto </h1>
+    <div className='App'>
+      {age >= 18 ? <h1>OVER AGE</h1> : <h1> UNDER AGE</h1>}
+      <h1 style={{ color: isPurple ? "purple" : "orange" }}> THIS HAS COLOR</h1>
+    
+      {isPurple && <button> BUTTON</button>}
     </div>
-  );
+    );
 }
 
 export default App
