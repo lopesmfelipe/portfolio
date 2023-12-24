bc3727bd0e5bb4e646214c66d0fd2e350fe55025
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-12-24 19:30:33 -0400

    Counter that increase, decrease and set to zero, and change their colors at the same time

--- NEO20/src/App.jsx
+++ NEO20/src/App.jsx
@@ -1,20 +1,52 @@
 import "./App.css";
-import { useState } from "react";
+import { useState} from "react";
 
 function App() {
-  const [textColor, setTextColor] = useState("lightpink"); 
+  const [count, setCount] = useState(0);
 
+  const increaseCount = () => { setCount(count + 1); };
+  const decreaseCount = () => { setCount(count - 1); };
+  const setToZero = () => { setCount(0); };
+
+  const [buttonColor, setButtonColor] = useState("lightskyblue");
+
+  const changeColor1 = () => { setButtonColor("lightsteelblue");};
+  const changeColor2 = () => { setButtonColor("lightPink"); };
+  const changeColor3 = () => { setButtonColor("yellow"); };
 
   return (
     <div className="App">
-      <button 
+      <button
+        style={{ backgroundColor: buttonColor, transition: 500}}
         onClick={() => {
-          setTextColor(textColor === "lightpink" ? "lightblue" :  "lightpink");
+          increaseCount();
+          changeColor1();
         }}
       >
-        Change-Colors
+        Increase
       </button>
-      <h1 style={{color: textColor}}> HELLO HUMAN BEING</h1>
+
+      <button
+        style={{ backgroundColor: buttonColor}}
+        onClick={() => {
+          decreaseCount();
+          changeColor2();
+        }}
+      >
+        Decrease
+      </button>
+
+      <button
+        style={{ backgroundColor: buttonColor}}
+        onClick={() => {
+          setToZero();
+          changeColor3();
+        }}
+      >
+        Set to Zero
+      </button>
+      
+      {count}
     </div>
   );
 }
