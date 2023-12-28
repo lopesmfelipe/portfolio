f38089919383c32637c268c6da66f024e06171ef
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-12-28 01:43:35 -0400

    Change task color

--- NEO20/src/App.jsx
+++ NEO20/src/App.jsx
@@ -14,9 +14,12 @@ function App() {
     const task = {
       id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1].id + 1,
       taskName: newTask,
+      completed: false
     };
     setToDoList([...toDoList, task]);
   };
+
+    
 
   const deleteTask = (id) => {
     setToDoList(toDoList.filter((task) => task.id !== id));
@@ -39,6 +42,7 @@ function App() {
               <Task
                 taskName={task.taskName}
                 id={task.id}
+                completed={task.completed}
                 deleteTask={deleteTask}
               />
             );
