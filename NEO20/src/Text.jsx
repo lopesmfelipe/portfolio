134eb432d5a3b48df0847678f31436b06e1544d5
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-12-29 13:16:58 -0400

    UseEffect Hook

--- NEO20/src/Text.jsx
+++ NEO20/src/Text.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const Text = () => {
   const [text, setText] = useState("");
+
+  useEffect(() => {
+    console.log("COMPONENT MOUNTED");
+
+    return () => {
+      console.log("COMPONENT UNMOUNTED");
+    };
+  }, []);
 
   return (
     <div>
