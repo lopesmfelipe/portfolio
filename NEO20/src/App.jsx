f81603f5060a5009bb831afffb0783e802764a9e
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-12-28 23:51:02 -0400

    Complete Task button fixed

--- NEO20/src/App.jsx
+++ NEO20/src/App.jsx
@@ -20,14 +20,25 @@ function App() {
   };
 
     
-
   const deleteTask = (id) => {
     setToDoList(toDoList.filter((task) => task.id !== id));
   };
 
+  const completeTask = (id) => {
+    setToDoList(
+      toDoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true};
+        } else {
+          return task;
+        }
+      })
+    );
+  }
+
+
   return (
     <div className="App">
-      <div className="main-container">
         <div className="addTask">
           <input onChange={handleChange} />
           <button className="button-addTask" onClick={addTask}>
@@ -44,71 +55,13 @@ function App() {
                 id={task.id}
                 completed={task.completed}
                 deleteTask={deleteTask}
+                completeTask={completeTask}
               />
             );
           })}
         </div>
-      </div>
     </div>
   );
 }
 
 export default App;
-
-/* import "./App.css";
-import { useState} from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  const increaseCount = () => { setCount(count + 1); };
-  const decreaseCount = () => { setCount(count - 1); };
-  const setToZero = () => { setCount(0); };
-
-  const [buttonColor, setButtonColor] = useState("lightskyblue");
-
-  const changeColor1 = () => { setButtonColor("lightsteelblue");};
-  const changeColor2 = () => { setButtonColor("lightPink"); };
-  const changeColor3 = () => { setButtonColor("yellow"); };
-
-  return (
-    <div className="App">
-      <button
-        style={{ backgroundColor: buttonColor, transition: 500}}
-        onClick={() => {
-          increaseCount();
-          changeColor1();
-        }}
-      >
-        Increase
-      </button>
-
-      <button
-        style={{ backgroundColor: buttonColor}}
-        onClick={() => {
-          decreaseCount();
-          changeColor2();
-        }}
-      >
-        Decrease
-      </button>
-
-      <button
-        style={{ backgroundColor: buttonColor}}
-        onClick={() => {
-          setToZero();
-          changeColor3();
-        }}
-      >
-        Set to Zero
-      </button>
-      
-      {count}
-    </div>
-  );
-}
-
-export default App;
-
-
-*/
--- NEO20/src/Task.jsx
+++ NEO20/src/Task.jsx
@@ -1,32 +1,14 @@
-import { useState } from "react";
 
 export const Task = (props) => {
 
-  const setBooleanTask = () => {
-    setToDoList(toDoList.map((task) => {
-      task.completed = true;
-      return task;
-    }));
-  };
-
-  const [taskColor, setColorTask] = useState('white');
-
-  const changeTaskColor = () => {
-    setColorTask('green');
-  }
-
-  const changeIt = () => {
-    if (props.completed === true) {
-      changeTaskColor();
-    }
-  }
-
-
   return (
-    <div>
-      <h1 style={{ color: taskColor}}>{props.taskName}</h1>
-      <button className="delete-button" onClick={() => props.deleteTask(props.id)}>XXX</button>
-      <button onClick={() => { setBooleanTask(); changeIt(); }}> Complete </button>
+    <div
+      className="task"
+      style={{ backgroundColor: props.completed ? "green" : "white"}}
+    >
+      <h1>{props.taskName}</h1>
+      <button onClick={() => props.completTask(props.id) }> Complete </button>
+      <button onClick={() => props.deleteTask(props.id)}>X</button>
     </div>
   ); 
 };
