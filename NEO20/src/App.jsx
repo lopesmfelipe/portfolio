aabd9cc29939da8f21afbaf3ee71cb8046a0f695
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-12-30 04:42:32 -0400

    Update

--- NEO20/src/App.jsx
+++ NEO20/src/App.jsx
@@ -2,16 +2,7 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
- // fetch("https://catfact.ninja/fact")
- //   .then((myResponse) => myResponse.json())
- //   .then((data) => {
- //     console.log(data);
- //   });
 
-
-    axios.get("https://catfact.ninja/fact").then((res) => {
-      console.log(res.data);
-    });
   return (
     <div className="App">
       <button> Generate some cat fact</button>
