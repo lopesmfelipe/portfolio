a88a06c358b12b9b2344f645fa849178b9c6713d
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2024-01-03 05:58:31 -0400

    Fetch cat fact function

--- NEO20/src/App.jsx
+++ NEO20/src/App.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
+  const [myCatFact, setMyCatFact] = useState("");
+  
+  const fetchCatFact = () => {
+    axios.get("https://catfact.ninja/fact").then((resp) => {
+      setMyCatFact(resp.data.fact);
+    });
+  }
+
+
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+    
 
   return (
     <div className="App">
-      <button> Generate some cat fact</button>
-      <p></p>
+      <button onClick={fetchCatFact}> Generate Cat Fact</button>
+      <p> {myCatFact} </p>      
     </div>
   );
 }
