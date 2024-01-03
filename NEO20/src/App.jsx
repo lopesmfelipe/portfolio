4ec21b4d29dd0e8081d6e6ec94114eac26f8ae12
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2024-01-03 06:37:43 -0400

    Predict Age button

--- NEO20/src/App.jsx
+++ NEO20/src/App.jsx
@@ -1,26 +1,30 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [myCatFact, setMyCatFact] = useState("");
-  
-  const fetchCatFact = () => {
-    axios.get("https://catfact.ninja/fact").then((resp) => {
-      setMyCatFact(resp.data.fact);
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
+
+  const fetchData = () => {
+    axios.get(`https://api.agify.io/?name=${name}`).then((resp) => {
+      setPredictedAge(resp.data);
     });
-  }
-
-
-  useEffect(() => {
-    fetchCatFact();
-  }, []);
-    
+  };
 
   return (
     <div className="App">
-      <button onClick={fetchCatFact}> Generate Cat Fact</button>
-      <p> {myCatFact} </p>      
+      <input
+        placeholder="Type any name and we will predict it"
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <button onClick={fetchData}> Predict the Age</button>
+
+      <h1> Name = {predictedAge?.name}</h1>
+      <h1> Predicted Age = {predictedAge?.age}</h1>
+      <h1> Count = {predictedAge?.count}</h1>
     </div>
   );
 }
