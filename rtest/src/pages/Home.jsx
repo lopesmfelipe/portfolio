4e94cfebaadfac259acb907a8cec68b1f8a8c1e2
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2024-01-07 07:17:06 -0400

    'data' to 'catdata'

--- rtest/src/pages/Home.jsx
+++ rtest/src/pages/Home.jsx
@@ -2,11 +2,22 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export const Home = () => {
-  const { data } = useQuery([""], () => {
+  const { data: catdata, isLoading, isError, refetch } = useQuery([""], () => {
     return axios.get("https://catfact.ninja/fact").then((res) => res.data);
   });
 
+  if (isError) {
+    return <h1> Sorry, there was an error</h1>
+  }
+
+  if (isLoading) {
+    return <h1> Loading...</h1>
+  }
+
   return (
-    <h1> Home Page <p> {data?.fact} </p> </h1>
+    <>
+      <h1> Home Page <p> {catdata?.fact} </p> </h1>
+      <button onClick={refetch}> Update Data</button>
+    </>
   );
 };
