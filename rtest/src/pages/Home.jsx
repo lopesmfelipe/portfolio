230c61076382a3fcfc01ecbd3e6dd7b812772175
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2024-01-07 06:51:26 -0400

    React query and axios

--- rtest/src/pages/Home.jsx
+++ rtest/src/pages/Home.jsx
@@ -2,17 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export const Home = () => {
-  const { data, isLoading } = useQuery(["cat"], () => {
+  const { data } = useQuery([""], () => {
     return axios.get("https://catfact.ninja/fact").then((res) => res.data);
   });
 
-  if (isLoading) {
-    return <h1> Loading...</h1>
-  }
-
   return (
-    <h1>
-      Home Page <p>{data?.fact}</p>
-    </h1>
+    <h1> Home Page <p> {data?.fact} </p> </h1>
   );
 };
