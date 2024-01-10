f67051ec0e6410a1bef2028f1011c79d63df3cbe
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2024-01-10 00:13:05 -0400

    Hook counter optimized

--- rtest/src/App.jsx
+++ rtest/src/App.jsx
@@ -1,35 +1,18 @@
 import "./App.css";
 import { Cat } from "./components/Cat";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useCount } from "./components/useCount";
 
 function App() {
-  
-  return (
-
-    <div>
-      <button onClick={increase}> increase </button>
-      <button onClick={increase}> increase </button>
-      <button onClick={increase}> increase </button>
-    </div>
-  );
-  
-  /*
-  const client = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: true,
-      },
-    },
-  });
-
+  const { count, increase, decrease, restart } = useCount();
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-        <Cat />
-      </QueryClientProvider>
+      {count}
+      <button onClick={increase}> increase </button>
+      <button onClick={decrease}> decrease </button>
+      <button onClick={restart}> restart </button>
     </div>
   );
-   */
-};
+}
 
 export default App;
---
+++ rtest/src/components/useCount.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export const useCount = (initialVal = 0) => {
+  const [count, setCount] = useState(initialVal);
+
+  const increase = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const decrease = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  const restart = () => {
+    setCount((prev) => 0);
+  };
+
+  return { count, increase, decrease, restart };
+};
