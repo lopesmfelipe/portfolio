4d2e0884484ec4da0f732f17465d86115f4c4096
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2024-01-14 23:50:01 -0400

    Deleted config

--- social-media/src/config/firebase.ts
+++ social-media/src/config/firebase.ts
@@ -6,16 +6,6 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyARwjAHTmRbpzD10bmSvtifHmKyjcRRmCo",
-  authDomain: "react-social-media-7ff63.firebaseapp.com",
-  projectId: "react-social-media-7ff63",
-  storageBucket: "react-social-media-7ff63.appspot.com",
-  messagingSenderId: "1045328089677",
-  appId: "1:1045328089677:web:5ab628ae668c1f658dc204",
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
